8f49ceb17535c8c08697c2ca25e0c6565e777132
bryansun96 <63320624+bryansun96@users.noreply.github.com>
2020-09-27 14:32:10 -0400

    Update US04and05Test.java

--- project/US04and05Test.java
+++ project/US04and05Test.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
-
+//Test by Bin Sun
 class US04and05Test {
 
 	@org.junit.jupiter.api.Test

78080fbc641141e54243b9e17ed3d7efce988310
donglin1111 <407218800@qq.com>
2020-10-03 15:35:03 -0400

    US06and07
    US06and07

---
+++ project/US06and07.java
@@ -0,0 +1,116 @@
+import java.util.List;
+import java.util.Map;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class US06and07 {
+	//Divorce can only occur before death of both spouses
+	public static boolean US06(List<Family> familyList,List<Individual> individualList, Map<String, Individual> individualMap){
+		boolean res = true;
+		for (Family family : familyList) {
+			if(!family.getDivorced().equals("NA")) {
+				String divStr = family.getDivorced();
+				String husbandID = family.getHusbandID();
+				String wifeID = family.getWifeID();
+				Individual husband,wife;
+				
+				if(husbandID != null) {
+					husband = individualMap.get(husbandID);
+					if(!husband.getIsAlive()) {
+						String husbandDeathStr = husband.getDeath();
+						if(!isDateValid(husbandDeathStr, divStr)) {
+							System.out.println("ERROR: FAMILY: US06: " + family.getLine() + ": " + family.getID() + ": Divorced " + family.getDivorced() 
+							+ " after husband's (" + husband.getId() + ") death on " + husband.getDeath()); 
+							res = false;
+						}
+						
+					}
+				}
+				if(wifeID != null) {
+					wife = individualMap.get(wifeID);
+					if(!wife.getIsAlive()) {
+						String wifeDeathStr = wife.getDeath();
+						if(!isDateValid(wifeDeathStr, divStr)) {
+							System.out.println("ERROR: FAMILY: US06: " + family.getLine() + ": " + family.getID() + ": Divorced " + family.getDivorced() 
+							+ " after wife's (" + wife.getId() + ") death on " + wife.getDeath()); 
+							res = false;
+						}
+						
+					}
+				}
+			}
+		}
+		return res;
+	}
+	
+	//Death should be less than 150 years after birth for dead people, and current date should be 
+	//less than 150 years after birth for all living people
+	public static boolean US07(List<Individual> individualList) {
+		boolean res = true;
+		for(Individual person: individualList) {
+			String birthStr = person.getBirthday();
+			String yearbirthStr = birthStr.substring(0,4);
+			int yearbirthInt = Integer.parseInt(yearbirthStr);
+			String monthbirthStr = birthStr.substring(5,7);
+			int monthbirthInt = Integer.parseInt(monthbirthStr);
+			String daybirthStr = birthStr.substring(8);
+			int daybirthInt = Integer.parseInt(daybirthStr);
+			LocalDate birthDate = LocalDate.of(yearbirthInt, monthbirthInt, daybirthInt);
+			// Death
+			if(!person.getDeath().equals("NA")) {
+				String deathStr = person.getDeath();
+				// year month day
+				String yeardeathStr = deathStr.substring(0,4);
+				int yeardeathInt = Integer.parseInt(yeardeathStr);
+				String monthddeathStr = deathStr.substring(5,7);
+				int monthdeathInt = Integer.parseInt(monthddeathStr);
+				String daydeathStr = deathStr.substring(8);
+				int daydeathInt = Integer.parseInt(daydeathStr);
+				LocalDate deathDate = LocalDate.of(yeardeathInt, monthdeathInt, daydeathInt);
+				Period p = Period.between(birthDate, deathDate);
+				if(p.getYears() > 150) {
+					System.out.println("ERROR: INDIVIDUAL: US07: " + person.getLine() + ": " + person.getId() + ": More than 150 years old at death - Brith " +
+							birthDate + ": Death " + deathDate ); 
+					res = false;
+				}
+			} else {//Alive
+				LocalDate today = LocalDate.now();
+				Period p = Period.between(birthDate, today);
+				if(p.getYears() > 150) {
+					System.out.println("ERROR: INDIVIDUAL: US07: " + person.getLine() + ": " + person.getId() + ": More than 150 years old at death - Brith " +
+							birthDate); 
+					res = false;
+				}
+			}
+		}
+		return res;
+	}
+	
+	public static boolean isDateValid(String endStr, String StartStr) {
+		boolean flag = false;;
+		String yearstartStr = StartStr.substring(0,4);
+		String yearendStr = endStr.substring(0,4);
+		int yearstartInt = Integer.parseInt(yearstartStr);
+		int yearendInt = Integer.parseInt(yearendStr);
+		String monthstartStr = StartStr.substring(5,7);
+		String monthendStr = endStr.substring(5,7);
+		int monthBirthInt = Integer.parseInt(monthstartStr);
+		int monthendInt = Integer.parseInt(monthendStr);
+		String daystartStr = StartStr.substring(8);
+		String dayendStr = endStr.substring(8);
+		int dayBirthInt = Integer.parseInt(daystartStr);
+		int dayendInt = Integer.parseInt(dayendStr);
+		if(yearendInt > yearstartInt) {
+			flag = true;
+		}else if(yearendInt == yearstartInt) {
+			if(monthBirthInt < monthendInt) {
+				flag = true;
+			}else if(monthBirthInt == monthendInt) {
+				if(dayBirthInt <= dayendInt) {
+					flag = true;
+				}
+			}
+		}		
+		return flag;
+	}
+}
---
+++ project/US06and07Test.java
@@ -0,0 +1,104 @@
+//import org.junit.Test;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import junit.framework.TestCase;
+
+public class US06and07Test extends TestCase {
+	//test1
+	Family f1 = new Family();
+	Individual I1 = new Individual();
+	Individual I2 = new Individual();
+	List<Individual> individualList1 = new ArrayList<Individual>();
+	List<Family> familyList1 = new ArrayList<Family>();
+	Map<String, Individual> map1 = new HashMap<String, Individual>();;
+	//test2
+	Family f2 = new Family();
+	Individual I12 = new Individual();
+	Individual I22 = new Individual();
+	List<Individual> individualList2 = new ArrayList<Individual>();
+	List<Family> familyList2 = new ArrayList<Family>();
+	Map<String, Individual> map2 = new HashMap<String, Individual>();;
+    protected void setUp() {
+        new US06and07();
+        //test 1 setUp
+		String id1 = "I1";
+		String id2 = "I2";
+		String fid1 = "F1";
+		String divdate = "1112-01-1";
+		String birthday1 = "1365-01-01";
+		String birthday2 = "1965-01-01";
+		I1.setAlive(true);
+		I1.setId(id1);
+		I1.setDeath("NA");
+		I2.setAlive(true);
+		I2.setId(id2);
+		I2.setDeath("NA");
+		I1.setBirthday(birthday1);
+		I2.setBirthday(birthday2);
+		individualList1.add(I1);
+		individualList1.add(I2);
+		map1.put(id1, I1);
+		map1.put(id2, I2);
+		f1.setID(fid1);
+		f1.setDivorced(divdate);
+		f1.setHusbandId(id1);
+		f1.setWifeId(id2);
+		familyList1.add(f1);
+		//test 2 setUp
+		String divdate2 = "2112-01-1";
+		String deathdate2 = "2110-01-1";
+		I12.setAlive(false);
+		I12.setDeath(deathdate2);
+		I12.setId(id1);
+		I22.setAlive(false);
+		I22.setDeath(deathdate2);
+		I22.setId(id2);
+		I12.setBirthday(birthday1);
+		I22.setBirthday(birthday2);
+		individualList2.add(I12);
+		individualList2.add(I22);
+		map2.put(id1, I12);
+		map2.put(id2, I22);
+		f2.setID(fid1);
+		f2.setDivorced(divdate2);
+		f2.setHusbandId(id1);
+		f2.setWifeId(id2);
+		familyList2.add(f2);		
+    }
+    //US06
+    public void test1() {
+        Assert.assertTrue(US06and07.US06(familyList1, individualList1, map1));
+      }
+    public void test2() {
+        Assert.assertEquals(US06and07.US06(familyList1, individualList1, map1),true);
+      }
+    public void test3() {
+        Assert.assertEquals(US06and07.US06(familyList2, individualList2, map2),false);
+      }
+    public void test4() {
+        Assert.assertNotEquals(US06and07.US06(familyList2, individualList2, map2),true);
+      }
+    public void test5() {
+        Assert.assertNotEquals(US06and07.US06(familyList2, individualList2, map2),US06and07.US06(familyList1, individualList1, map1));
+      }
+    //US07
+    public void test6() {
+        Assert.assertFalse(US06and07.US07(individualList1));
+      }
+    public void test7() {
+        Assert.assertNotEquals(US06and07.US07(individualList1),true);
+      }
+    public void test8() {
+        Assert.assertFalse(US06and07.US07(individualList2));
+      }
+    public void test9() {
+        Assert.assertNotEquals(US06and07.US07(individualList2),true);
+      }
+    public void test10() {
+        Assert.assertEquals(US06and07.US07(individualList2),US06and07.US07(individualList1));
+      }
+}

a553f0e92e93d46680903fc110a718828e8adfb3
weijiep <62127049+weijiep@users.noreply.github.com>
2020-10-23 12:05:25 -0400

    add new functions
    add setLastName(), getLastName() in inidividual class
    add setHusbandLastName(), getHusbandLastName in family class.

--- project/Family.java
+++ project/Family.java
@@ -1,68 +1,104 @@
+package src;
+
 import java.util.List;
 
 public class Family {
-	
+
 	private String ID;
 	private int line;
 	private String married;
 	private String divorced;
 	private String husbandID;
 	private String husbandName;
+	private String husbandLastName;
 	private String wifeID;
 	private String wifeName;
 	private List<Individual> children;
-	
+
 	public String getID() {
 		return ID;
 	}
-	public int getLine() {return line;}
+
+	public int getLine() {
+		return line;
+	}
+
 	public String getMarried() {
 		return married;
 	}
+
 	public String getDivorced() {
 		return divorced;
 	}
+
 	public String getHusbandID() {
 		return husbandID;
 	}
+
 	public String getHusbandName() {
 		return husbandName;
 	}
+	
+	
+	public String getHusbandLastName() {
+		return husbandLastName;
+	}
+	
+	
 	public String getWifeID() {
 		return wifeID;
 	}
+
 	public String getWifeName() {
 		return wifeName;
 	}
+
 	public List<Individual> getChildren() {
 		return children;
 	}
-	
+
 	// mutators
 	public void setID(String ID) {
 		this.ID = ID;
 	}
-	public void setLine(int line) { this.line = line; }
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
 	public void setMarried(String married) {
 		this.married = married;
 	}
+
 	public void setDivorced(String divorced) {
 		this.divorced = divorced;
 	}
+
 	public void setHusbandId(String husbandID) {
 		this.husbandID = husbandID;
 	}
+
 	public void setHusbandName(String husbandName) {
 		this.husbandName = husbandName;
 	}
+
+	
+	
+	public void setHusbandLastName(String husbandLastName) {
+		this.husbandLastName = husbandLastName;
+	}
+	
+	
+	
 	public void setWifeId(String wifeID) {
 		this.wifeID = wifeID;
 	}
+
 	public void setWifeName(String wifeName) {
 		this.wifeName = wifeName;
 	}
+
 	public void setChildren(List<Individual> children) {
 		this.children = children;
 	}
-
 }
--- project/Individual.java
+++ project/Individual.java
@@ -1,8 +1,11 @@
-public class Individual {
+package src;
 
+public class Individual {
 	private String ID;
 	private int line;
 	private String name;
+	private String Firstname;
+	private String Lastname;
 	private String gender;
 	private String birthday;
 	private int age;
@@ -10,59 +13,101 @@ public class Individual {
 	private String death;
 	private String child;
 	private String spouse;
-	
+
 	public String getId() {
 		return ID;
 	}
-	public int getLine() {return line;}
+
+	public int getLine() {
+		return line;
+	}
+
 	public String getName() {
 		return name;
 	}
+	
+	public String getFirstName() {
+		return Firstname;
+	}
+	public String getLastName() {
+		return Lastname;
+	}
+
 	public String getGender() {
 		return gender;
 	}
+
 	public String getBirthday() {
 		return birthday;
 	}
+
 	public int getAge() {
 		return age;
 	}
-	public boolean getIsAlive() { return alive; }
+
+	public boolean getIsAlive() {
+		return alive;
+	}
+
 	public String getDeath() {
 		return death;
 	}
+
 	public String getChild() {
-			return child;
+		return child;
 	}
+
 	public String getSpouse() {
-			return spouse;
+		return spouse;
 	}
-	//mutators;
+
+	// mutators;
 	public void setId(String ID) {
 		this.ID = ID;
 	}
-	public void setLine(int line) { this.line = line; }
+
+	public void setLine(int line) {
+		this.line = line;
+	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	
+	public void setFirstName(String FirstName) {
+		this.Firstname = Firstname;
+	}
+	
+	public void setLastName(String Lastname) {
+		this.Lastname = Lastname;
+	}
+	
+	
 	public void setGender(String gender) {
 		this.gender = gender;
 	}
+
 	public void setBirthday(String birthday) {
 		this.birthday = birthday;
 	}
+
 	public void setAge(int age) {
 		this.age = age;
 	}
+
 	public void setAlive(boolean alive) {
 		this.alive = alive;
 	}
+
 	public void setDeath(String death) {
 		this.death = death;
 	}
+
 	public void setChild(String child) {
 		this.child = child;
 	}
+
 	public void setSpouse(String spouse) {
 		this.spouse = spouse;
 	}
